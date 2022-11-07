603c03b014e807c424d0ff8e282182a6d2125f42
Mihail <uleman@mail.ru>
2022-11-07 23:10:58 +0300

    tbl

--- static/bfu.js
+++ static/bfu.js
@@ -42,8 +42,8 @@ function onLoad() {
       });
 
     document.querySelector(idEl).innerHTML = curFile.name + ", " + Math.floor(startingByte / 1024) + " Kb, " 
-        + Math.floor(startingByte * 100 / curFile.size) + "%, ошибок " + errorCount + ", "
-        + elapsed + " m, "+ formatter.format(curDate);
+        + Math.floor(startingByte * 100 / curFile.size) + "%, ошибок " + errorCount 
+        + ", осталось" + elapsed + " минут, " + formatter.format(curDate);
 
     if (startingByte < curFile.size) {
 
